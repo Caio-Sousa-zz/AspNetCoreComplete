dbc4a2366099d5aabad0e57babf48c8d872f668a
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2021-01-19 19:57:29 -0300

    EF Mappings.

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,36 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    class MeuDbContext: DbContext
+    {
+        public MeuDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+  
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties()
+            .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            // Register mappings
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            // Disable cascade delete
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
