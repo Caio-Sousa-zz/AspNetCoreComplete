45c4ff531bf4e85bc47017f05d8f2b83195f5226
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2021-02-10 22:07:16 -0300

    Refatoração

--- src/DevIO.Business/Notificacoes/Notificacao.cs
+++ src/DevIO.Business/Notificacoes/Notificacao.cs
@@ -1,8 +1,4 @@
-﻿using DevIO.Business.Interfaces;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace DevIO.Business.Notificacoes
+﻿namespace DevIO.Business.Notificacoes
 {
     public class Notificacao
     {
@@ -11,31 +7,6 @@ namespace DevIO.Business.Notificacoes
         public Notificacao(string mensagem)
         {
             Mensagem = mensagem;
-        }
-    }
-
-    public class Notificador : INotificador
-    {
-        private List<Notificacao> _notificacoes;
-
-        public Notificador()
-        {
-            _notificacoes = new List<Notificacao>();
-        }
-
-        public void Handle(Notificacao notificao)
-        {
-            _notificacoes.Add(notificao);
-        }
-
-        public List<Notificacao> ObterNotificacao()
-        {
-            return _notificacoes;
-        }
-
-        public bool TemNotificacao()
-        {
-            return _notificacoes.Any();
         }
     }
 }
