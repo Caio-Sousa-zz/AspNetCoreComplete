1cd8e68bbf188cade6d4289cfb4256b4a0113a64
Caio César Silva Sousa <caiyo.projeto@gmail.com>
2021-02-16 16:06:56 -0300

    Adicionar nova camada de segurança.

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -22,7 +22,7 @@ namespace DevIO.App
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-             
+
             services.AddIndentityConfig(Configuration);
 
             services.AddDbContext<MeuDbContext>(options =>
@@ -39,7 +39,7 @@ namespace DevIO.App
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-             if (env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
@@ -53,6 +53,7 @@ namespace DevIO.App
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
